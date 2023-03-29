d6548d38558f87923f2b192c466f0cbb0fdcd454
bothomthit <125108405+bothomthit@users.noreply.github.com>
2023-03-29 21:24:12 +0700

    Create lab6.b1.java

---
+++ lab6.b1.java
@@ -0,0 +1,8 @@
+public class Bai1 {
+    public static void main(String[] args) {
+        ChuyenXeNoiThanh CXNT= new ChuyenXeNoiThanh("45", 5000, "45A","Tai", "45K-59",45555);
+        CXNT.Xuat();
+        ChuyenXeNgoaiThanh CXNgT=new ChuyenXeNgoaiThanh("HaNoi", 12, "H45", "Tuan", "56AD" , 456);
+        CXNgT.Xuat();
+    }
+}
